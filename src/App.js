744744ddad650695bc30c3ad8c0976e07ea5ca83
Lucas Lins <lucaslinsperfil@gmail.com>
2022-09-19 13:40:50 -0300

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@
 import './App.css';
 import {Routes,Route} from 'react-router-dom';
 import Home from './pages';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import SignUp from './pages/SignUp';
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/forgot-password' element={<ForgotPassword/>}/>
+      <Route path='/offers' element={<Offers/>}/>
     </Routes>
   );
 }
